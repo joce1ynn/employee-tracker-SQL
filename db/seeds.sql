15a6df8a0e843b364fd3b5e2c4b3ad941cd28bae
Wenwen Tian <wwtian9@gmail.com>
2022-01-22 21:40:14 -0700

    add employee data

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,23 @@ INSERT INTO departments (name)
 VALUES
 ("Sales"),
 ("Technology"),
-("Marketing"),
 ("Legal");
+
+INSERT INTO roles (title, salary, department_id) 
+VALUES
+("Sales Manager", "120000", 1),
+("Sales Representative", "70000", 1),
+("IT Manager", "130000", 2),
+("Software Developer", "100000", 2),
+("General Counsel", "110000", 3),
+("Legal Assistant", "60000", 3);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+VALUES
+("Katelyn","Booth",1,null),
+("Hasan","Mcintosh",2,1),
+("Taha","Jones",3,null),
+("Jerry","Lynch",4,3),
+("Sylvie","Riley",5,null),
+("Abdullah","Wiggins",6,5);
+
